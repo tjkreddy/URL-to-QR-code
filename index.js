0f3697123db90daaf8b18bdd1926ab57fb77a5fb
Jugal Kishore Reddy Thangella <143167588+tjkreddy@users.noreply.github.com>
2025-03-08 19:02:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+import qr from "qr-image";
+import fs from "fs";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req,res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/convert", (req,res) => {
+    const url = req.body.URL;
+    const qr_svg = qr.image(url, { type: 'png' });
+    const qrPath = __dirname + "/public/qr.png";
+    
+    const writeStream = fs.createWriteStream(qrPath);
+    qr_svg.pipe(writeStream);
+    
+    writeStream.on('finish', () => {
+      res.sendFile(qrPath);
+    });
+
+    writeStream.on('error', (err) => {
+      console.error('Error writing QR code:', err);
+      res.status(500).send('Error generating QR code');
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
